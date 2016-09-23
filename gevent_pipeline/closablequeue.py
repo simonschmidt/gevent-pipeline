329e082f1fbbce4e943ef0c885fae57266847022
Simon Schmidt <schmidt.simon@gmail.com>
2016-09-23 23:52:58 +0300

    More reliable unsticker for getters
    Previous version didn't take maxsize into account

--- gevent_pipeline/closablequeue.py
+++ gevent_pipeline/closablequeue.py
@@ -38,6 +38,21 @@ class ClosableQueue(queue.Queue):
     def closed(self):
         return self._closed
 
+    def _unstick(self):
+        """
+        Unstick all blocking getters
+        """
+        if not self._closed:
+            raise RuntimeError("Don't start unsticking before closing")
+
+        gevent.sleep()
+        while len(self.getters) > len(self.putters):
+            gevent.sleep()
+            try:
+                super().put(StopIteration, block=False)
+            except queue.Full:
+                pass
+
     def close(self, once=True):
         """
         Close the queue
@@ -50,36 +65,22 @@ class ClosableQueue(queue.Queue):
             raise RuntimeError("Tried closing already closed queue")
 
         self._closed = True
-
-        # Unstick all waiting getters, no need to see if there are any putters
-        # as there is no harm in a few extra StopIterations.
-        #
-        # Has assumptions:
-        # 1. No interruption between setting _closed and obtaining getters
-        #     - OK! Just simple objects, no place where gevent switches
-        # 2. No interruption in .get between _closed check and beieng added to self.getters
-        #     - OK! Either nonblocking or __get_or_peek which appends to getters before waiting
-        # 3. That any .put always ends up before StopIteration
-        #     - Ok! Either put resolves without blocking or wakes getters only or is appended
-        #       to putters before blocking. So there is no time for _closed to change between
-        #       self.put checking it and the put ending up in an ok situation.
-        for _ in range(len(self.getters)):
-            self._fuzz()
-            super().put(StopIteration)
+        gevent.spawn(self._unstick)
 
     def put(self, item, *args, **kwargs):
-        kwargs.get('block', True) and self._fuzz()
 
         if self._closed:
             raise RuntimeError("Cannot put to a closed queue")
 
-        return super().put(item, *args, **kwargs)
+        result = super().put(item, *args, **kwargs)
+        kwargs.get('block', True) and self._fuzz()
+        return result
 
     def get(self, *args, **kwargs):
-        kwargs.get('block', True) and self._fuzz()
-
         if not self._closed:
-            return super().get(*args, **kwargs)
+            item = super().get(*args, **kwargs)
+            kwargs.get('block', True) and self._fuzz()
+            return item
 
         try:
             # Note: cannot use super().get_nowait here as that will just call this function again
